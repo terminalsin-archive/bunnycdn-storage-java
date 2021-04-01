edf7d3d6760374495fd46550aad9364666502a20
Thibaut Gautier <shanyujuneja@gmail.com>
2021-04-01 19:46:39 +0200

    Inital commit

--- src/main/java/net/bunny/api/BunnyClient.java
+++ src/main/java/net/bunny/api/BunnyClient.java
@@ -7,8 +7,8 @@ public interface BunnyClient {
      * Provides a new Storage API manager which handles all connection linked
      * to BunnyCDN storage. This simplifies a lot of the handling and is just
      * overall a cute thing.
-     * @param region
-     * @return
+     * @param region Region for the CDN
+     * @return Storage API manager
      */
     StorageAPI getStorageAPI(String region);
 }
